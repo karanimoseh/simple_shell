e63efb1db5b65221e5cae101d58e4ac0a875e322
karanimoseh <karanimoseh@gmail.com>
2022-12-21 10:32:45 -0800

    tasks

---
+++ main.c
@@ -0,0 +1,49 @@
+#include "shell.h"
+
+/**
+ * main - The function that starts the shell
+ *
+ * Return: 1 on success
+ */
+
+int main(void)
+{
+	int status = 1;
+	char *line;
+	char **args;
+
+	signal(SIGINT, sigint_handler);
+	while (status)
+	{
+		status = isatty(STDIN_FILENO);
+
+		if (status == 1)
+			write(STDOUT_FILENO, "$ ", 2);
+
+		line = readline();
+		if (!line)
+		{
+			return (0);
+		}
+		/* Exit command we will have to move it */
+		if (_strcmp(line, "env") == 0)
+		{
+			_printenv();
+			free(line);
+			continue;
+		}
+		args = splitline(line);
+		/* check if args is NULL(malloc failed) */
+		if (args == NULL)
+		{
+			free(line);
+			free(args);
+			continue;
+		}
+		/* Execute the commands given by user */
+		status = execute(args);
+		free(line);
+		free(args);
+	}
+	return (0);
+}
